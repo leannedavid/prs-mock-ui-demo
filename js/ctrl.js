47d841afa962cd3bd7d50e080c17e5420ed46c1e
leannedavid <leanne.skit@gmail.com>
2017-09-16 19:00:56 -0700

    Implemented sorting feature on table

--- js/ctrl.js
+++ js/ctrl.js
@@ -1,36 +1,55 @@
 webdevapp.controller('MyWebDevController', ['$scope', function($scope){
+    $scope.sortType = 'fileName';
+    $scope.sortReverse = false;
 
     $scope.fileList =[
       {
-      'fileName': 'Blah blah blah.jpg',
-      'filePath': '/photos/test1.jpg',
-      'lastModified': 'Forever Ago',
-      'uploadedBy': 'Me',
-      'status': 'Pending',
-      'comments': 'Initial upload here'
-    },
+        'fileName': 'Blah blah blah.jpg',
+        'filePath': '/photos/test1.jpg',
+        'lastModified': 'Forever Ago',
+        'uploadedBy': 'Me',
+        'status': 'Pending',
+        'comments': 'Initial upload here'
+      },
 
-    {
-    'fileName': 'Pls.jpg',
-    'filePath': '/photos/test2.jpg',
-    'lastModified': 'Yesterday',
-    'uploadedBy': 'Not Me',
-    'status': 'Approved',
-    'comments': 'Added something to it'
-  }]
+      {
+        'fileName': 'Pls.jpg',
+        'filePath': '/photos/test2.jpg',
+        'lastModified': 'Yesterday',
+        'uploadedBy': 'Not Me',
+        'status': 'On Hold',
+        'comments': 'Added something to it'
+      },
+
+      {
+        'fileName': 'Ok.jpg',
+        'filePath': '/photos/test1.jpg',
+        'lastModified': 'Some time ago',
+        'uploadedBy': 'Welp',
+        'status': 'Needs Approval',
+        'comments': 'Trying to test'
+      },
+
+      {
+        'fileName': 'A file.docx',
+        'filePath': '/photos/test2.jpg',
+        'lastModified': 'Now',
+        'uploadedBy': 'Main Admin',
+        'status': 'Approved',
+        'comments': 'Document has been approved.'
+      }
+
+
+  ]
 
   $scope.dzOptions = {
-    //paramName : 'photo',
     maxFilesize : '10',
     uploadMultiple: 'no'
-    //previewTemplate:
-
-
   }
 
   $scope.dzCallbacks = {
     'addedfile' : function(file){
-      console.info('File added from dropzone 1.', file);
+      console.info('File added.', file);
     }
   }
 
